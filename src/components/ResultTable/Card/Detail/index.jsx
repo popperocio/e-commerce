f189702563b6173b5516c949c15e8690e5d462b2
rocio <rociocpoppe@gmail.com>
2024-02-21 14:09:03 -0300

    feat: use rate values from the api to replace the fixed values on the UI

--- src/components/ResultTable/Card/Detail/index.jsx
+++ src/components/ResultTable/Card/Detail/index.jsx
@@ -2,12 +2,12 @@ import { AddToCartButton } from "../../../Button/AddToCartButton"
 import { Rating } from "../../../Filter/RatingFilter/Rating"
 import './Detail.css'
 
-function Detail ({title, price }) {
+function Detail ({title, price, rating }) {
     return (
         <>
             <div className="DetailsCardContainer">
                 <h3>{title}</h3>
-                <Rating stars={3} />
+                <Rating stars={rating} />
                 <h3>${price}</h3>
                 <div className="AddToCartButtonContainer">
                     <AddToCartButton></AddToCartButton> 
