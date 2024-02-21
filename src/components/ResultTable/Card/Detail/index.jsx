228c3106f983b6c1e8d40d5aea70514ee5620d95
rocio <rociocpoppe@gmail.com>
2024-02-21 15:06:59 -0300

    fix: fix product card size

--- src/components/ResultTable/Card/Detail/index.jsx
+++ src/components/ResultTable/Card/Detail/index.jsx
@@ -4,17 +4,13 @@ import './Detail.css'
 
 function Detail ({title, price }) {
     return (
-        <>
             <div className="DetailsCardContainer">
-                <h3>{title}</h3>
+                <h3 className="DetailsProductTitle">{title}</h3>
                 <Rating stars={3} />
                 <h3>${price}</h3>
-                <div className="AddToCartButtonContainer">
-                    <AddToCartButton></AddToCartButton> 
-                </div>
+                <AddToCartButton className="AddToCartButton"></AddToCartButton> 
             </div>
             
-        </>
     )
 }
 
