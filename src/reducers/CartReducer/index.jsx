f2d76a77c9667e09223f1fd790b9cc863cc5ac0b
rocio <rociocpoppe@gmail.com>
2024-02-25 22:45:55 -0300

    fix: remove commented out code

--- src/reducers/CartReducer/index.jsx
+++ src/reducers/CartReducer/index.jsx
@@ -10,16 +10,6 @@ const initialState = {
             ...state,
             toggleCartDetail: !state.toggleCartDetail,
         }
-      // case "ADD_TO_CART":
-      //   return {
-      //     ...state,
-      //     cartProducts: [...state.cartProducts, action.payload],
-      //   };
-      // case "REMOVE_FROM_CART":
-      //   return {
-      //     ...state,
-      //     cartProducts: state.cartProducts.filter(product => product.id !== action.payload.id),
-      //   };
       default:
           return state;
     }
