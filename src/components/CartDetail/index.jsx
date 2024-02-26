cb6338283e5f83a89570dcd1546698665384261c
rocio <rociocpoppe@gmail.com>
2024-02-26 18:33:55 -0300

    feat: add increase and decrease buttons in the cart detail

--- src/components/CartDetail/index.jsx
+++ src/components/CartDetail/index.jsx
@@ -2,10 +2,20 @@ import './CartDetail.css'
 import { useContext } from 'react';
 import { SearchContext } from '../../contexts/SearchContext';
 import { ContinueButton } from '../Button/ContinueButton';
+import { IncreaseButton } from '../Button/IncreaseButton';
+import { DecreaseButton } from '../Button/DecreaseButton';
 
 function CartDetail () {
 
-  const { cartState } = useContext(SearchContext);
+  const { cartState, dispatch } = useContext(SearchContext);
+
+  const setProductQuantity = (productId, newQuantity) => {
+    dispatch({
+      type: "UPDATE_PRODUCT_QUANTITY",
+      payload: { productId, quantity: newQuantity }
+    });
+  };
+
 
   return (
       <div className={cartState.toggleCartDetail ? "CartDetailContainerShow" : "CartDetailContainerHide"}>
@@ -17,8 +27,12 @@ function CartDetail () {
             {cartState.cartProducts.map((product) => (
               <div key={product.id} className="CartProductContainer">
                   <img src={product.image}/>
-                  <p>${product.price}</p>
-                  <h3>{ product.quantity }</h3>
+                  <h3>${product.price}</h3>
+                  <div className='IncreaseAndDecreaseButtons'>
+                    <DecreaseButton quantity={product.quantity} setQuantity={(newQuantity) => setProductQuantity(product.id, newQuantity)} />
+                    <h3>{ product.quantity }</h3>
+                    <IncreaseButton quantity={product.quantity} setQuantity={(newQuantity) => setProductQuantity(product.id, newQuantity)}/> 
+                  </div>
               </div>
               ))}
         </div>
